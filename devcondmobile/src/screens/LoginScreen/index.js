a570f33f6803162c243d635a622fdd280fb282d4
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-01-21 23:03:56 -0300

    view preload

---
+++ devcondmobile/src/screens/LoginScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import C from './style';
+
+export default () => {
+
+    return(
+        <C.Container>
+            <C.Texto>Tela de login</C.Texto>
+        </C.Container>  
+    );
+};
--- devcondmobile/src/stacks/AuthStack.js
+++ devcondmobile/src/stacks/AuthStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Preload from '../screens/PreloadScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,12 @@ export default () => {
            <Stack.Screen 
                 name="PreloadScreen"
                 component={Preload}
+                options={{headerShown:false}}
+           />
+
+            <Stack.Screen 
+                name="LoginScreen"
+                component={LoginScreen}
                 options={{headerShown:false}}
            />
        </Stack.Navigator> 
