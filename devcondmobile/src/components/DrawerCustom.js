719f6046cc91c276e683bb63872dbb5bc23ead05
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-01-30 16:55:16 -0300

    drawer custom

--- devcondmobile/src/components/DrawerCustom.js
+++ devcondmobile/src/components/DrawerCustom.js
@@ -4,6 +4,7 @@ import {useNavigation} from '@react-navigation/native';
 import {useStateValue} from '../context/StateContext';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../services/api'
 
 const DrawerArea = styled.View`
     flex:1;
@@ -61,10 +62,42 @@ const FooterUnitText = styled.Text`
 `;
 const FooterUnitButton = styled.TouchableOpacity``;
 
+const MenuButton = styled.TouchableOpacity`
+    flex-direction:row;
+    margin-bottom:5px;
+    border-radius:5px;
+    align-items:center;
+`;
+
+const MenuSquare =  styled.View`
+    width:5px;
+    height:35px;
+    margin-right:20px;
+    background-color: transparent;
+    border-top-right-radius:5px;
+    border-bottom-right-radius:5px;
+`;
+
+const MenuButtonText = styled.Text`
+    font-size:15px;
+    margin-left:10px;
+    color:#666e78;
+`;
+
 export default (props) => {
 
     const navigation = useNavigation();
     const[context,dispatch] = useStateValue();
+
+    const menus = [
+        {title:'Mural de avisos', icon:'inbox', screen:'WallScreen'},
+        {title:'Documentos', icon:'file-text', screen:'DocumentScreen'},
+        {title:'Reservas', icon:'calendar', screen:'ReservationScreen'},
+        {title:'Livro de ocorrencias', icon:'bug', screen:'WarningScreen'},
+        {title:'Achados e Perdidos', icon:'search', screen:'FoundLostScreen'},
+        {title:'Boletos', icon:'wpforms', screen:'BilletScreen'},
+        {title:'Perfil', icon:'user', screen:'PrfileScreen'},
+    ]
 
     const handleChangeUnit = async () => {
         
@@ -78,12 +111,37 @@ export default (props) => {
          });
     }
 
+    const handleLogout = async  () => {
+        await api.logout();
+
+        navigation.reset({
+            index:1,
+            routes:[{
+                name:'LoginScreen'
+            }]
+        });
+    }
+
     return(
         <DrawerArea>
             <DrawerLogoArea>
                 <DrawerLogo source={require('../assets/undraw_home.png')} resizeMode="contain" />
             </DrawerLogoArea>
+                {
+                    menus.map((item,index) => (
+                        <MenuButton key={index} onPress={() => navigation.navigate(item.screen)}>
+                            <MenuSquare></MenuSquare>
+                            <Icon name={item.icon} size={20} color={'#666e78'}/>
+                            <MenuButtonText>{item.title}</MenuButtonText>
+                        </MenuButton>
+                    ))
+                }
 
+                        <MenuButton onPress={handleLogout}>
+                            <MenuSquare></MenuSquare>
+                            <Icon name="toggle-left" size={20} color={'#666e78'}/>
+                            <MenuButtonText>Sair</MenuButtonText>
+                        </MenuButton>
             <DrawerScroller>
 
             </DrawerScroller>
