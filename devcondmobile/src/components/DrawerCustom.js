bdb4c0b289c8bb2ad914e5b07c0b622d36812e7e
powermonitoramento <desenvolvimento@organizacoespower.com.br>
2021-01-30 14:32:09 -0300

    drawer custom

--- devcondmobile/src/components/DrawerCustom.js
+++ devcondmobile/src/components/DrawerCustom.js
@@ -3,28 +3,62 @@ import  styled from 'styled-components/native';
 import {useNavigation} from '@react-navigation/native';
 import {useStateValue} from '../context/StateContext';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const DrawerArea = styled.View`
     flex:1;
-    background-color: #ff0000;
+    background-color: #fff;
 `;
 
-const DrawerLogoArea = styled.View``;
+const DrawerLogoArea = styled.View`
+    padding: 10px 20px;
+    border-bottom-width:1px;
+    border-bottom-color: #eee;
+`;
 
-const DrawerLogo = styled.Image ``;
+const DrawerLogo = styled.Image `
+    width:190px;
+    height:40px;
+`;
 
-const DrawerScroller = styled.ScrollView``;
+const DrawerScroller = styled.ScrollView`
+    flex:1;
+    margin:20px 0;
+`;
 
-const ChangeUnitArea = styled.View``;
+const ChangeUnitArea = styled.View`
+    margin:10px;
+`;
 
-const ChangeUnitButton = styled.TouchableOpacity``;
+const ChangeUnitButton = styled.TouchableOpacity`
+    background-color:#8863e6;
+    padding:12px;
+    justify-content:center;
+    align-items:center;
+    border-radius:5px;
+`;
 
-const ChangeUnitButtonText = styled.Text``;
+const ChangeUnitButtonText = styled.Text`
+    color:#fff;
+    font-size:15px;
+    font-weight:bold;
+`;
 
-const FooterArea = styled.View``;
+const FooterArea = styled.View`
+    padding:20px;
+    flex-direction:row;
+    justify-content:space-between;
+    align-items:center;
+`;
 const FooterInfo = styled.View``;
-const FooterProfile = styled.Text``;
-const FooterUnitText = styled.Text``;
+const FooterProfile = styled.Text`
+    font-size:15px;
+    color:#000;
+`;
+const FooterUnitText = styled.Text`
+    font-size:15px;
+    color:#666e78;
+`;
 const FooterUnitButton = styled.TouchableOpacity``;
 
 export default (props) => {
@@ -32,6 +66,17 @@ export default (props) => {
     const navigation = useNavigation();
     const[context,dispatch] = useStateValue();
 
+    const handleChangeUnit = async () => {
+        
+         await AsyncStorage.removeItem('property');
+
+         navigation.reset({
+             index:1,
+             routes:[{
+                 name:'ChoosePropertScreen'
+             }]
+         });
+    }
 
     return(
         <DrawerArea>
@@ -44,17 +89,17 @@ export default (props) => {
             </DrawerScroller>
 
             <ChangeUnitArea>
-                <ChangeUnitButton>
+                <ChangeUnitButton onPress={handleChangeUnit}>
                     <ChangeUnitButtonText>Trocar unidade</ChangeUnitButtonText>
                 </ChangeUnitButton>
             </ChangeUnitArea>
 
             <FooterArea>
                 <FooterInfo>
-                    <FooterProfile>Saulo</FooterProfile>
-                    <FooterUnitText>propriedade</FooterUnitText>
+                    <FooterProfile>olá {context.user.user.name}</FooterProfile>
+                    <FooterUnitText>{context.user.property.name}</FooterUnitText>
                 </FooterInfo>
-                <FooterUnitButton>
+                <FooterUnitButton onPress={() => navigation.navigate('UnitScreen')}>
                     <Icon name="gear" size={24} color="#66e78" />
                 </FooterUnitButton>
             </FooterArea>
